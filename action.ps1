1bd2cb0316bf07d9d37b614eef3563565eb1fb70
rhouthuijzen <116062840+rhouthuijzen@users.noreply.github.com>
2025-10-08 17:44:06 +0200

    Feature: added partner solution id in header

--- action.ps1
+++ action.ps1
@@ -16,8 +16,9 @@ try {
     $base64 = [System.Convert]::ToBase64String($bytes)
     $key = "Basic $base64"
     $headers = @{
-        "authorization" = $Key
-        "Accept"        = "application/json"
+        "authorization"       = $Key
+        "Accept"              = "application/json"
+        "Partner-Solution-Id" = "TOOL001"
     }
 
     Write-Verbose "Archiving TOPdesk Person for: [$($userDisplayName)]"
